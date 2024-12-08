84b9d8c1fcf159e93a0b71a918f7e19ea97dac0f
Giselle <gisellesouz.ti@gmail.com>
2024-12-08 10:16:16 -0300

    Adjust new layout for project

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,43 @@
-import { Open_Sans } from 'next/font/google';
 import Provider from './provider';
+import Navbar from './components/Navbar';
+import { Outfit } from 'next/font/google';
 
-const openSans = Open_Sans({
+import { Box } from '@chakra-ui/react';
+
+const outfit = Outfit({
   subsets: ['latin'],
+  weights: ['400', '600', '700'],
   display: 'swap',
-  weight: ['400', '600', '700'],
 });
 
 export default function RootLayout({ children }) {
   return (
-    <html className={openSans.className} suppressHydrationWarning>
+    <html
+      style={{ height: '100%', overflow: 'hidden' }}
+      suppressHydrationWarning
+      className={outfit.className}
+    >
       <head />
       <body>
-        <Provider>{children}</Provider>
+        <Provider>
+          <Box
+            maxW={{ base: '95%', md: '800px', lg: '1200px' }}
+            mx="auto"
+            mt="10"
+            p="6"
+            boxShadow="lg"
+            borderRadius="md"
+            height="90vh"
+            overflow="hidden"
+            display="flex"
+            flexDirection="column"
+          >
+            <Navbar />
+            <Box flex="1" overflow="auto">
+              {children}
+            </Box>
+          </Box>
+        </Provider>
       </body>
     </html>
   );
