9c7305beab4325f2a0feb0ebd049545b7fc74a3d
Giselle <gisellesouz.ti@gmail.com>
2024-12-09 12:53:49 -0300

    Add react-query lib and configure layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,13 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 import Provider from './provider';
 import Navbar from './components/Navbar';
 import { Outfit } from 'next/font/google';
 
 import { Box } from '@chakra-ui/react';
+import { IntlProviderWrapper } from '@/context/IntlContext';
 
 const outfit = Outfit({
   subsets: ['latin'],
@@ -10,7 +15,9 @@ const outfit = Outfit({
   display: 'swap',
 });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, params }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <html
       style={{ height: '100%', overflow: 'hidden' }}
@@ -18,26 +25,31 @@ export default function RootLayout({ children }) {
       className={outfit.className}
     >
       <head />
+
       <body>
-        <Provider>
-          <Box
-            maxW={{ base: '95%', md: '800px', lg: '1200px' }}
-            mx="auto"
-            mt="10"
-            p="6"
-            boxShadow="lg"
-            borderRadius="md"
-            height="90vh"
-            overflow="hidden"
-            display="flex"
-            flexDirection="column"
-          >
-            <Navbar />
-            <Box flex="1" overflow="auto">
-              {children}
-            </Box>
-          </Box>
-        </Provider>
+        <QueryClientProvider client={queryClient}>
+          <IntlProviderWrapper>
+            <Provider>
+              <Box
+                maxW={{ base: '95%', md: '800px', lg: '1200px' }}
+                mx="auto"
+                mt="10"
+                p="6"
+                boxShadow="lg"
+                borderRadius="md"
+                height="90vh"
+                overflow="hidden"
+                display="flex"
+                flexDirection="column"
+              >
+                <Navbar />
+                <Box flex="1" overflow="auto">
+                  {children}
+                </Box>
+              </Box>
+            </Provider>
+          </IntlProviderWrapper>
+        </QueryClientProvider>
       </body>
     </html>
   );
