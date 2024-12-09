a758b3ddd63fe945107ce22102c8450fb9fddbc0
Giselle <gisellesouz.ti@gmail.com>
2024-12-09 13:01:16 -0300

    Set initial Home page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import { Box, Heading } from '@chakra-ui/react';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Box mx="auto" mt="10" p="6" boxShadow="lg" borderRadius="md">
+        <Heading as="h1" size="2xl" mb="4">
+          GoLedger Streaming
+        </Heading>
+      </Box>
+    </>
+  );
 }
