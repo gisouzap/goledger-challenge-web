4772aec1771a5c88c847ee8f85657ab587723e63
Giselle <gisellesouz.ti@gmail.com>
2024-12-09 17:03:23 -0300

    Implement home page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,35 @@
-import { Box, Heading } from '@chakra-ui/react';
+'use client';
+
+import { Box, Heading, Separator, Text, VStack } from '@chakra-ui/react';
+import { useIntl } from 'react-intl';
 
 export default function Home() {
+  const { formatMessage } = useIntl();
+
   return (
     <>
-      <Box mx="auto" mt="10" p="6" boxShadow="lg" borderRadius="md">
-        <Heading as="h1" size="2xl" mb="4">
-          GoLedger Streaming
-        </Heading>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="100%"
+        p={4}
+      >
+        <VStack spacing={6} padding={8} textAlign="center">
+          <Heading as="h1" size="2xl" color="purple.700">
+            {formatMessage({ id: 'greeting' })}
+          </Heading>
+          <Separator mb="6" mt="6" />
+          <Text fontSize="lg" color="gray.500" maxW="600px">
+            {formatMessage({ id: 'projectExperience' })}
+          </Text>
+          <Text fontSize="lg" color="gray.500">
+            {formatMessage({ id: 'exploreNavbar' })}
+          </Text>
+          <Text fontSize="sm" mt="6" color="gray.500">
+            {formatMessage({ id: 'thankYou' })}
+          </Text>
+        </VStack>
       </Box>
     </>
   );
